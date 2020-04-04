b4600861b16f7d0ae487238100c9e95fce7b41a0
JosueLoboCR <60111482+JosueLoboCR@users.noreply.github.com>
2020-04-03 20:55:54 -0600

    Add files via upload

---
+++ DataAccess/ConnectionToSql.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    public abstract class ConnectionToSql
+    {
+        //private readonly string connectionString;
+        //public ConnectionToSql()
+        //{
+        //    connectionString = "Server=RJCODEADVANCE;DataBase= MyCompany; integrated security= true";
+        //}
+        //protected SqlConnection GetConnection()
+        //{
+        //    return new SqlConnection(connectionString);
+        //}
+    }
+}
---
+++ DataAccess/UserDao.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+//using System.Data.SqlClient;
+
+
+namespace DataAccess
+{
+   public class UserDao:ConnectionToSql
+    {
+    //    public bool Login(string user, string pass)
+    //    {
+    //        using (var connection = GetConnection()){
+    //            connection.Open();
+    //            using (var command = new SqlCommand())
+    //            {
+    //                command.Connection = connection;
+    //                command.CommandText = "select *from Users where LoginName=@user and Password=@pass";
+    //                command.Parameters.AddWithValue("@user", user);
+    //                command.Parameters.AddWithValue("@pass", pass);
+    //                command.CommandType = CommandType.Text;
+    //                SqlDataReader reader = command.ExecuteReader();
+    //                if (reader.HasRows)
+    //                {
+                        
+    //                    return true;
+    //                }
+    //                else
+    //                    return false;
+    //            }
+    //        }
+    //    }
+
+  }
+}
